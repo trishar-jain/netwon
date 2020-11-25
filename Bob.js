eea1597bc6f4bf8bed1c40a645326846198e3f5d
trishar-jain <71705284+trishar-jain@users.noreply.github.com>
2020-11-25 09:55:55 +0530

    Add files via upload

--- Bob.js
+++ Bob.js
@@ -1,28 +1,23 @@
 class Bob {
-    constructor(x,y,radius) {
-        var opt = {
-            isStatic:false,
-            'resitution':33,
-            'isStatic': false,
-            'density': 0.25
-            
-        }
-        this.x = x;
-        this.y = y;
-        this.body = Bodies.circle(x,y,radius,opt);
-
-        World.add(world,this.body);
+    constructor(x, y, diameter) {
+      var options = {
+            isStatic: false,
+            restitution: 1.2,
+            friction: 0.4,
+            density: 1.5
+      }
+    this.body = Bodies.circle(x, y, diameter/2, options);
+    this.diameter = diameter;
+      
+    World.add(world, this.body);
     }
-
-    display() {
+    display(){
         var pos = this.body.position;
-        var angle = this.body.angle;
         push();
-        translate(pos.x,pos.y);
-        rotate(angle);
-        ellipseMode(RADIUS);
-        fill("blue");
-        ellipse(0,0,this.body.circleRadius,this.body.circleRadius);
+        translate(pos.x, pos.y);
+        rectMode(CENTER);
+        fill("red");
+        ellipse(0, 0, this.diameter);
         pop();
     }
-}
+  };
--- Rope.js
+++ Rope.js
@@ -1,24 +1,31 @@
 class Rope {
-    constructor(bodyA,bodyB,offsetX,offsetY) {
+    constructor(body1, body2, offsetX, offsetY) {
         this.offsetX = offsetX;
         this.offsetY = offsetY;
-        var opt = {
-            bodyA: bodyA,
-            bodyB: bodyB,
-            pointB: {x:this.offsetX,y:this.offsetY},
-            stiffness:0.04
-        }
-        this.rope = Constraint.create(opt);
-        World.add(world,this.rope);
-    }
 
+        var options = {
+            bodyA: body1,
+            bodyB: body2,
+            pointB:{x: this.offsetX, y: this.offsetY}
+        }
+
+        this.rope = Matter.Constraint.create(options);
+        World.add(world, this.rope);
+
+    }
     display() {
         var pointA = this.rope.bodyA.position;
         var pointB = this.rope.bodyB.position;
-        strokeWeight(2);
-        push();
-        fill("white");
-        line(pointA.x,pointA.y, pointB.x+this.offsetX, pointB.y+this.offsetY);
-        pop();
+
+        var Anchor1X = pointA.x;
+        var Anchor1Y = pointA.y;
+ 
+        var Anchor2X = pointB.x + this.offsetX;
+        var Anchor2Y = pointB.y + this.offsetY;
+
+        strokeWeight(5);
+        stroke("yellow");
+
+        line(Anchor1X, Anchor1Y, Anchor2X, Anchor2Y);
     }
-}
+}  
